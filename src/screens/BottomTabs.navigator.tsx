b8657ab67e01bf0a3b7b19ca60553211f5f6bd0f
Alex Aymkin <aymkin@gmail.com>
2022-03-19 21:41:10 +0200

    Use a vector icons for on all tab

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -4,15 +4,48 @@ import { HomeScreen } from './Home.screen'
 import { HistoryScreen } from './History.screen'
 import { AnalyticsScreen } from './Analytics.screen'
 import { SCREENS } from './constants/screens'
+import { AnalyticsIcon, HistoryIcon, HomeIcon } from '../components/Icon/Icons'
+import { colors } from '../theme/colors'
 
 const BottomTabs = createBottomTabNavigator()
 
 export const BottomTabsNavigator: React.FC = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name={SCREENS.Home} component={HomeScreen} />
-      <BottomTabs.Screen name={SCREENS.History} component={HistoryScreen} />
-      <BottomTabs.Screen name={SCREENS.Analytics} component={AnalyticsScreen} />
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.blue,
+        tabBarInactiveTintColor: colors.grey,
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home')
+            return <HomeIcon color={color} size={size} />
+
+          if (route.name === 'History') {
+            return <HistoryIcon color={color} size={size} />
+          }
+
+          if (route.name === 'Analytics') {
+            return <AnalyticsIcon color={color} size={size} />
+          }
+
+          return null
+        },
+      })}>
+      <BottomTabs.Screen
+        name={SCREENS.Home}
+        component={HomeScreen}
+        options={{ title: "Today's Mood" }}
+      />
+      <BottomTabs.Screen
+        name={SCREENS.History}
+        component={HistoryScreen}
+        options={{ title: 'Past Moods' }}
+      />
+      <BottomTabs.Screen
+        name={SCREENS.Analytics}
+        component={AnalyticsScreen}
+        options={{ title: 'TFancy Charts' }}
+      />
     </BottomTabs.Navigator>
   )
 }
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,4 +3,6 @@ export const colors = {
   white: '#fff',
   darkText: '#242424',
   lavender: '#87677B',
+  blue: '#1D84B5',
+  grey: '#8E9AAF',
 }
