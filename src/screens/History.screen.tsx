c9df7a86ac2334ca7a323c3330b779605d448048
Alex Aymkin <aymkin@gmail.com>
2022-03-19 20:22:30 +0200

    setup app context

--- src/screens/History.screen.tsx
+++ src/screens/History.screen.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Text } from '../components/Text'
+import { useAppContext } from '../App.provider'
 
 export const HistoryScreen: React.FC = () => {
+  const appContext = useAppContext()
   return (
     <View style={styles.container}>
       <Text>HistoryScreen</Text>
+      <Text>{appContext.greeting}</Text>
     </View>
   )
 }
