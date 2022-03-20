acadfaa8a8fddd8e6cab2153d3e59ee2eb1314c4
Alex Aymkin <aymkin@gmail.com>
2022-03-20 12:13:15 +0200

    Use react-native-gesture-handler to swipe row

--- src/components/MoodItemRow/MoodItemRow.tsx
+++ src/components/MoodItemRow/MoodItemRow.tsx
@@ -7,6 +7,13 @@ import { MoodOptionWithTimestamp } from '../../types'
 import { colors } from '../../theme/colors'
 import { fonts } from '../../theme/fonts'
 import { useAppContext } from '../../App.provider'
+import { PanGestureHandler } from 'react-native-gesture-handler'
+import Reanimated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  useAnimatedGestureHandler,
+} from 'react-native-reanimated'
 
 type MoodItemRowProps = {
   item: MoodOptionWithTimestamp
@@ -14,25 +21,49 @@ type MoodItemRowProps = {
 
 export const MoodItemRow: React.FC<MoodItemRowProps> = ({ item }) => {
   const appContext = useAppContext()
+  const offset = useSharedValue(0)
 
   const handlePress = React.useCallback(() => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.linear)
     appContext.handleDeleteMood(item)
   }, [appContext, item])
 
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: offset.value }],
+  }))
+
+  const onGestureEvent = useAnimatedGestureHandler(
+    {
+      onActive: event => {
+        const xVal = Math.floor(event.translationX)
+        offset.value = xVal
+      },
+      onEnd: () => {
+        offset.value = withTiming(0)
+      },
+    },
+    [],
+  )
+
   return (
-    <View style={styles.moodItem}>
-      <View style={styles.iconAndDescription}>
-        <Text style={styles.moodValue}>{item.mood.emoji}</Text>
-        <Text style={styles.moodDescription}>{item.mood.description}</Text>
-      </View>
-      <Text style={styles.moodDate}>
-        {format(new Date(item.timestamp), "dd MMM, yyyy 'at' h:mmaaa")}
-      </Text>
-      <PressableArea hitSlop={16} onPress={handlePress}>
-        <Text style={styles.deleteText}>Delete</Text>
-      </PressableArea>
-    </View>
+    <PanGestureHandler
+      // @ts-expect-error This method is deprecated but supported for backward compatibility.
+      minDeltaX={1}
+      minDeltaY={100}
+      onGestureEvent={onGestureEvent}>
+      <Reanimated.View style={[styles.moodItem, animatedStyle]}>
+        <View style={styles.iconAndDescription}>
+          <Text style={styles.moodValue}>{item.mood.emoji}</Text>
+          <Text style={styles.moodDescription}>{item.mood.description}</Text>
+        </View>
+        <Text style={styles.moodDate}>
+          {format(new Date(item.timestamp), "dd MMM, yyyy 'at' h:mmaaa")}
+        </Text>
+        <PressableArea hitSlop={16} onPress={handlePress}>
+          <Text style={styles.deleteText}>Delete</Text>
+        </PressableArea>
+      </Reanimated.View>
+    </PanGestureHandler>
   )
 }
 
