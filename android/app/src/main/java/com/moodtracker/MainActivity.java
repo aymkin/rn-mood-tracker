c15fce9a6b2c7443ae17b3b03adc548fc7358bcc
Alex Aymkin <aymkin@gmail.com>
2022-03-20 11:53:06 +0200

    install react-native-gesture-handler

--- android/app/src/main/java/com/moodtracker/MainActivity.java
+++ android/app/src/main/java/com/moodtracker/MainActivity.java
@@ -3,6 +3,10 @@ package com.moodtracker;
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -13,6 +17,16 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "MoodTracker";
   }
+
+  @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+       @Override
+         protected ReactRootView createRootView() {
+         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
+    }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
